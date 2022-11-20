37f4e0f544826e1ca228ca7422b111ef5170501a
RozenNoureev <rozennoureev@gmail.com>
2022-11-19 23:18:22 -0600

    update organization

---
+++ categoryPages/movie.js
@@ -0,0 +1,9 @@
+//This program defines a movie object
+
+class movie {
+    constructor(title, image, description){
+        this.title=title; // String name
+        this.image=image; // link to the image jpeg
+        this.description = description; // link to description txt file
+    }
+}
