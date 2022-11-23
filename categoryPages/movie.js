ea6651820939144c9bf7926c66562a0d260d39e0
RozenNoureev <rozennoureev@gmail.com>
2022-11-22 18:24:15 -0600

    add fantasy page. minor tweaks.

--- categoryPages/movie.js
+++
@@ -1,9 +0,0 @@
-//This program defines a movie object
-
-class movie {
-    constructor(title, image, description){
-        this.title=title; // String name
-        this.image=image; // link to the image jpeg
-        this.description = description; // link to description txt file
-    }
-}
