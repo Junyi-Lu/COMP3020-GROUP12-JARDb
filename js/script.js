5794b194d7543de3609e3f7c95784f7956f74dcd
junkdeveloper <q906107506@gmail.com>
2022-11-26 00:46:41 -0600

    recent version

--- js/script.js
+++ js/script.js
@@ -21,6 +21,79 @@ const nope = new Movie("Nope", "Horror", "Sci-Fi", 3.5, "images/nope.jpeg", "Dan
 const theBatman = new Movie("The Batman", "Action", "Crime", 4.0, "images/thebatman.jpeg", "Robert Pattinson", "Zoe Kravitz", "Jeffrey Wright");
 
 let MovieList = [deadpool, americanSniper, gattaca, nope, theBatman];
+let arr = ["deadpool", "americansniper", "gattaca", "nope", "thebatman"];
+
+
+var search = document.getElementsByClassName("search-data")[0];
+var selectedid = document.getElementById("selectedid");
+
+function showList() {
+    var res = searchByIndexOf(search.value, arr);
+    for (var i = 0; i < res.length; i++) {
+        var a = document.createElement("a");
+        var images = document.createElement("img");
+        var li = document.createElement("li");
+        a.innerHTML = res[i];
+        if (res[i] == "deadpool") {
+            a.href = "moviePages/deadpool.html";
+            images.src = "images/deadpool.jpeg";
+        } else if (res[i] == "americansniper") {
+            a.href = "moviePages/main.html";
+            images.src = "images/americansniper.jpeg";
+        } else if (res[i] == "gattaca") {
+            a.href = "moviePages/main.html";
+            images.src = "images/gattaca.jpeg";
+
+        } else if (res[i] == "nope") {
+            a.href = "moviePages/main.html";
+            images.src = "images/nope.jpeg";
+        } else {
+            a.href = "moviePages/main.html";
+            images.src = "images/thebatman.jpeg";
+        }
+        a.appendChild(images);
+        document.getElementById("drop").appendChild(li);
+        li.appendChild(a);
+    }
+}
+
+search.oninput = function getMoreContents() {
+    var drop = document.getElementById("drop");
+    selectedid.removeChild(drop);
+
+    var originalUL = document.createElement("ul");
+    originalUL.id = "drop";
+    selectedid.appendChild(originalUL);
+    showList();
+}
+
+search.onfocus = function () {
+    var originalUL = document.createElement("ul");
+    originalUL.id = "drop";
+    selectedid.appendChild(originalUL);
+    showList();
+}
+
+
+
+function searchByIndexOf(keyWord, list) {
+    if (!(list instanceof Array)) {
+        return;
+    }
+    if (keyWord == "") {
+        return [];
+    } else {
+        var len = list.length;
+        var arr = [];
+        for (var i = 0; i < len; i++) {
+            let temp = keyWord.toLowerCase();
+            if (list[i].toLowerCase().indexOf(temp) >= 0) {
+                arr.push(list[i]);
+            }
+        }
+        return arr;
+    }
+}
 
 
 
@@ -32,25 +105,6 @@ function displayDemo() {
         let href = MovieList[i].title;
         href += ".html"
         demoList[i].href = href;
-    }
-}
-
-function search_movies() {
-    let input = document.getElementById("searchID").value;
-    input = input.toLowerCase();
-    let movieList = document.getElementsByClassName("movies");
-    console.log(input);
-    for (i = 0; i < MovieList.length; i++) {
-        if (!MovieList[i].title.toLowerCase().includes(input) || input == "" || input == " ") {
-            // console.log(MovieList[i].title);
-            // console.log("Cant find the item");
-        } else {
-            console.log(MovieList[i].title);
-            console.log("Found the item");
-            movieList[0].style.display = "block";
-            movieList[0].src = MovieList[i].img.src;
-            console.log(MovieList[i].img.src);
-        }
     }
 }
 
--- moviePages/scripts.js
+++ moviePages/scripts.js
@@ -15,30 +15,29 @@ var swiper = new Swiper(".swiper", {
     loop: true,
     breakpoints: {
         "@0.00": {
-        slidesPerView: 1,
-        spaceBetween: 10,
+            slidesPerView: 1,
+            spaceBetween: 10,
         },
         "@0.75": {
-        slidesPerView: 2,
-        spaceBetween: 20,
+            slidesPerView: 2,
+            spaceBetween: 20,
         },
         "@1.00": {
-        slidesPerView: 3,
-        spaceBetween: 40,
+            slidesPerView: 3,
+            spaceBetween: 40,
         },
         "@1.50": {
-        slidesPerView: 4,
-        spaceBetween: 50,
+            slidesPerView: 4,
+            spaceBetween: 50,
         },
     },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-        },
-    });
+    },
+});
 
-ratingButton.addEventListener("click", function()
-{
+ratingButton.addEventListener("click", function () {
     var options = document.querySelector(".ratingOptions");
 
     if (options.style.opacity == 1) // Ratings open, so close it
@@ -51,48 +50,40 @@ ratingButton.addEventListener("click", function()
     }
 });
 
-fiveStar.addEventListener("click", function()
-{
+fiveStar.addEventListener("click", function () {
     highlightStars(5);
     ratingButton.style.color = "rgb(233, 216, 67)";
 });
 
-fourStar.addEventListener("click", function()
-{
+fourStar.addEventListener("click", function () {
     highlightStars(4);
     ratingButton.style.color = "rgb(233, 216, 67)";
 });
 
-threeStar.addEventListener("click", function()
-{
+threeStar.addEventListener("click", function () {
     highlightStars(3);
     ratingButton.style.color = "rgb(233, 216, 67)";
 });
 
-twoStar.addEventListener("click", function()
-{
+twoStar.addEventListener("click", function () {
     highlightStars(2);
     ratingButton.style.color = "rgb(233, 216, 67)";
 });
 
-oneStar.addEventListener("click", function()
-{
+oneStar.addEventListener("click", function () {
     highlightStars(1);
     ratingButton.style.color = "rgb(233, 216, 67)";
 });
 
 
 //Highlight stars from 1 to rating
-function highlightStars(rating, isHigligted)
-{
-    for (let i = rating; i < 5; i++)
-    {
+function highlightStars(rating, isHigligted) {
+    for (let i = rating; i < 5; i++) {
         ratings[i].style.color = " rgb(61, 61, 61)";
     }
 
     // Highlight stars
-    for (let i = 0; i < rating; i++)
-    {
+    for (let i = 0; i < rating; i++) {
         ratings[i].style.color = "rgb(233, 216, 67)";
     }
 }
@@ -113,5 +104,80 @@ function display() {
         toTop.style.display = "block";
     } else {
         toTop.style.display = "none";
+    }
+}
+
+
+let arr = ["deadpool", "americansniper", "gattaca", "nope", "thebatman"];
+
+
+var search = document.getElementsByClassName("search-data")[0];
+var selectedid = document.getElementById("selectedid");
+
+function showList() {
+    var res = searchByIndexOf(search.value, arr);
+    for (var i = 0; i < res.length; i++) {
+        var a = document.createElement("a");
+        var images = document.createElement("img");
+        var li = document.createElement("li");
+        a.innerHTML = res[i];
+        if (res[i] == "deadpool") {
+            a.href = "moviePages/deadpool.html";
+            images.src = "../images/deadpool.jpeg";
+        } else if (res[i] == "americansniper") {
+            a.href = "moviePages/main.html";
+            images.src = "../images/americansniper.jpeg";
+        } else if (res[i] == "gattaca") {
+            a.href = "moviePages/main.html";
+            images.src = "../images/gattaca.jpeg";
+
+        } else if (res[i] == "nope") {
+            a.href = "moviePages/main.html";
+            images.src = "../images/nope.jpeg";
+        } else {
+            a.href = "moviePages/main.html";
+            images.src = "../images/thebatman.jpeg";
+        }
+        a.appendChild(images);
+        document.getElementById("drop").appendChild(li);
+        li.appendChild(a);
+    }
+}
+
+search.oninput = function getMoreContents() {
+    var drop = document.getElementById("drop");
+    selectedid.removeChild(drop);
+
+    var originalUL = document.createElement("ul");
+    originalUL.id = "drop";
+    selectedid.appendChild(originalUL);
+    showList();
+}
+
+search.onfocus = function () {
+    var originalUL = document.createElement("ul");
+    originalUL.id = "drop";
+    selectedid.appendChild(originalUL);
+    showList();
+}
+
+
+
+function searchByIndexOf(keyWord, list) {
+    if (!(list instanceof Array)) {
+        return;
+    }
+    if (keyWord == "") {
+        return [];
+    } else {
+        var len = list.length;
+        var arr = [];
+        for (var i = 0; i < len; i++) {
+            let temp = keyWord.toLowerCase();
+            if (list[i].toLowerCase().indexOf(temp) >= 0) {
+                arr.push(list[i]);
+            }
+        }
+        return arr;
     }
 }
